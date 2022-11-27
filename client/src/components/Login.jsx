ae792863c1ee54ce482b8bf0c23db8490e2d5381
Sakthivel Ravichandran <sakthivelravichandran18@gmail.com>
2022-11-27 15:37:22 -0500

    added login and signup template

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,6 +1,72 @@
+import React, { useState } from "react";
+import Snackbar from '@mui/material/Snackbar';
+
 const Login = () => {
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [snackbar, setSnackbar] = useState({
+        open: false,
+        message: ''
+    });
+
+    const handleLogin = () => {
+        if (!email || !password) {
+            return setSnackbar({
+                open: true,
+                message: 'Please enter both email and password'
+            });
+        }
+        console.log(email, password);
+    }
+    const handleClose = () => {
+        setSnackbar({
+            open: false,
+            message: ''
+        })
+    }
+    const SnackbarAlert = (
+        <React.Fragment>
+            <Snackbar
+                anchorOrigin={{vertical: 'top', horizontal: 'center'}}
+                open={snackbar.open}
+                severity='error'
+                autoHideDuration={6000}
+                onClose={handleClose}
+                message={snackbar.message}
+                key={'top' + 'center'}
+            />
+        </React.Fragment>
+    );
+
     return ( 
-        <h4>This is Login component</h4>
+        <div className="container-fluid mt-2">
+            <h3>Login</h3>
+            <div className="row">
+                <div className="col-md-4">
+                    <form>
+                        <div className="form-group row my-3">
+                            <label htmlFor="email" className="col-sm-2 col-form-label">Email</label>
+                            <div className="col-sm-10">
+                                <input type="text" className="form-control" id="email" onChange={e => setEmail(e.target.value)}/>
+                            </div>
+                        </div>
+                        <div className="form-group row my-3">
+                            <label htmlFor="password" className="col-sm-2 col-form-label">Password</label>
+                            <div className="col-sm-10">
+                                <input type="password" className="form-control" id="password" onChange={e => setPassword(e.target.value)} />
+                            </div>
+                        </div>
+                        <div className="d-flex flex-row-reverse m-2">
+                            <a className="text-primary" href="/signUp">Forgot password?</a>
+                        </div>
+                        <div className="d-flex flex-row-reverse">
+                            <button className="btn btn-primary" onClick={handleLogin}>Login</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            {SnackbarAlert}
+         </div>
      );
 }
  
--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,6 +1,83 @@
+import React, { useState } from "react";
+import Snackbar from '@mui/material/Snackbar';
+
 const Signup = () => {
+    const [firstName, setFirstName] = useState();
+    const [lastName, setLastName] = useState();
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [snackbar, setSnackbar] = useState({
+        open: false,
+        message: ''
+    });
+
+    const handleSignup = () => {
+        if (!email || !password || !firstName || !lastName) {
+            return setSnackbar({
+                open: true,
+                message: 'Please enter all the details'
+            });
+        }
+        console.log(email, password);
+    }
+    const handleClose = () => {
+        setSnackbar({
+            open: false,
+            message: ''
+        })
+    }
+    const SnackbarAlert = (
+        <React.Fragment>
+            <Snackbar
+                anchorOrigin={{vertical: 'top', horizontal: 'center'}}
+                open={snackbar.open}
+                severity='error'
+                autoHideDuration={6000}
+                onClose={handleClose}
+                message={snackbar.message}
+                key={'top' + 'center'}
+            />
+        </React.Fragment>
+    );
+
     return ( 
-        <h4>This is Signup component</h4>
+        <div className="container-fluid mt-2">
+            <h3>Signup</h3>
+            <div className="row">
+                <div className="col-md-4">
+                    <form>
+                        <div className="form-group row my-3">
+                            <label htmlFor="firstName" className="col-sm-4 col-form-label">First name</label>
+                            <div className="col-sm-8">
+                                <input type="text" className="form-control" id="firstName" onChange={e => setFirstName(e.target.value)}/>
+                            </div>
+                        </div>
+                        <div className="form-group row my-3">
+                            <label htmlFor="lastName" className="col-sm-4 col-form-label">Last name</label>
+                            <div className="col-sm-8">
+                                <input type="text" className="form-control" id="lastName" onChange={e => setLastName(e.target.value)}/>
+                            </div>
+                        </div>
+                        <div className="form-group row my-3">
+                            <label htmlFor="email" className="col-sm-4 col-form-label">Email</label>
+                            <div className="col-sm-8">
+                                <input type="text" className="form-control" id="email" onChange={e => setEmail(e.target.value)}/>
+                            </div>
+                        </div>
+                        <div className="form-group row my-3">
+                            <label htmlFor="password" className="col-sm-4 col-form-label">Password</label>
+                            <div className="col-sm-8">
+                                <input type="password" className="form-control" id="password" onChange={e => setPassword(e.target.value)} />
+                            </div>
+                        </div>
+                        <div className="d-flex flex-row-reverse">
+                            <button className="btn btn-primary" onClick={handleSignup}>Signup</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            {SnackbarAlert}
+         </div>
      );
 }
  
